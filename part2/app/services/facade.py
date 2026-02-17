0fb39c940b654a7009574be2bc676cdfd6cf9a3f
ahotb <ah.r9625@gmail.com>
2026-02-17 12:54:56 +0300

    add user retrieval and update methods in HBnBFacade class

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 
+
 class HBnBFacade:
     def __init__(self):
         self.user_repo = InMemoryRepository()
@@ -16,3 +17,24 @@ class HBnBFacade:
     def get_place(self, place_id):
         # Logic will be implemented in later tasks
         pass
+
+    def get_all_users(self):
+        """ Retrieve a list of all users """
+        return self.user_repo.get_all()
+
+    def update_user(self, user_id, user_data):
+        """ Updates user data using your User class """
+        user = self.get_user(user_id)
+        if not user:
+            return None
+
+        # Using the profileUpdate method from your class
+        # We use .get() to avoid errors if the user doesn't send all fields
+        user.profileUpdate(
+            firstName=user_data.get('firstName'),
+            lastName=user_data.get('lastName'),
+            email=user_data.get('email')
+        )
+
+        self.user_repo.update(user.id, user)
+        return user
