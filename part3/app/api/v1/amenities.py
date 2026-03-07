d4b065d834589db3aef3e8ffd1135d26d190e424
Mohammed2254 <m.a.alabduli@gmail.com>
2026-03-07 04:30:49 +0300

    add jwt validation to aminities

--- part3/app/api/v1/amenities.py
+++ part3/app/api/v1/amenities.py
@@ -1,7 +1,7 @@
 """Amenity API endpoints."""
 
 from flask_restx import Namespace, Resource, fields
-
+from flask_jwt_extended import jwt_required, get_jwt
 from app.services import facade
 
 api = Namespace('amenities', description='Amenity operations')
